722010043456dfe1b72e11e0835041e176fb0d23
Ahmed <ahmedalkhalaf@outlook.fr>
2024-11-02 15:07:47 +0100

    form

--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -29,7 +29,7 @@ export default function Contact() {
     <div id="contact" className="contact-container">
       <h2 className='contact-h2'>Contact</h2>
       <form ref={form} onSubmit={sendEmail} className="contact-form">
-        <label htmlFor="user_name">Npm</label>
+        <label htmlFor="user_name">Nom</label>
         <input type="text" id="user_name" name="user_name" required placeholder="Votre Nom" />
 
         <label htmlFor="user_email">Email</label>
